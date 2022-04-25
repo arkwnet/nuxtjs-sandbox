de911525cfdfd46b1a1a599f7edd0aed69c2debd
Sora Arakawa <mail@arkw.net>
2022-04-25 15:49:18 +0900

    Add project files

---
+++ pages/sub.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <p>Sub Page</p>
+    <p><NuxtLink to="/">Return to top page.</NuxtLink></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SubPage',
+}
+</script>
